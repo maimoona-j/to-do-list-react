9a95c1bc512e560a552bdb743be9b637136585ae
maimoona <maimoonaj97@gmail.com>
2023-04-25 22:40:01 +0500

    create basic structure of to-do-list app

---
+++ src/component/ToDoForm.js
@@ -0,0 +1,57 @@
+// import React from 'react'
+// import { useState } from 'react';
+
+// export default function ToDoForm() {
+//     const ToDoForm = ({ addTask }) => {
+//         const [ userInput, setUserInput ] = useState('');
+
+
+//         const handleChange = (e) => {
+//             setUserInput(e.currentTarget.value)
+//         }
+    
+//         const handleSubmit = (e) => {
+//             e.preventDefault();
+//             addTask(userInput);
+//             setUserInput("");
+      
+//         }
+    
+    
+//       return (
+//         <div>
+//           <form action="/">
+//           <input value={userInput} type="text" onChange={handleChange} placeholder="Enter task..."/>
+//           </form>
+//         </div>
+//       )
+
+//     }
+   
+// }
+
+
+import React, { useState } from 'react';
+
+const ToDoForm = ({ addTask }) => {
+
+    const [ userInput, setUserInput ] = useState('');
+
+    const handleChange = (e) => {
+        setUserInput(e.currentTarget.value)
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        addTask(userInput);
+        setUserInput("");
+    }
+    return (
+        <form onSubmit={handleSubmit}>
+            <input value={userInput} type="text" onChange={handleChange} placeholder="Enter task..."/>
+            <button>Submit</button>
+        </form>
+    );
+};
+
+export default ToDoForm;
